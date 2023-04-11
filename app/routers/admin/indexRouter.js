e267d59ecc5bbbd55bdf92c25df288e628ad2f8f
Suraj Vishwakarma <surajvishwakarma@protonmail.com>
2023-04-12 01:11:47 +0530

    012-04-2023

--- app/routers/admin/indexRouter.js
+++ app/routers/admin/indexRouter.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const admin = require('../../controllers/admin/indexController')
 
 router.get('/dashboard', admin.dashboard)
-router.get('/upload-menu', admin.getMenuPage)
+router.get('/create-question', admin.getMenuPage)
 router.post('/upload-menu', admin.uploadMenu)
 module.exports = router;
